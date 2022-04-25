85584d1c8dc6b06d37d190e362a8e4c3455d6c30
TLCurd <TLCurdChiro@gmail.com>
2022-04-25 17:52:33 -0600

    add working forEach loop to populate markers for all 14ers

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,26 @@
 
 <script>
+import axios from 'axios'
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      peaks: []
     };
   },
   mounted: function () {
+    this.peaksIndex();
     this.makeMap();
   },
   methods: {
+    peaksIndex: function () {
+      console.log('indexing Peaks...');
+      axios.get(`http://localhost:3000/peaks.json`).then(response => {
+        console.log(response.data);
+        this.peaks = response.data
+      })
+    },
     makeMap: function () {
       mapboxgl.accessToken = process.env.VUE_APP_MAP_KEY;
       const map = new mapboxgl.Map({
@@ -19,12 +29,19 @@ export default {
         center: [-105.634722, 38.985833], // starting position [lng, lat]
         zoom: 7 // starting zoom
       });
-      const marker = new mapboxgl.Marker({
-        color: "red",
-        rotation: 0
-      })
-        .setLngLat([-106.475700, 39.187500])
-        .addTo(map);
+      axios.get(`http://localhost:3000/peaks.json`).then(response => {
+        console.log(response.data);
+        this.peaks = response.data;
+        this.peaks.forEach(peak => {
+          console.log(peak.name)
+          const marker = new mapboxgl.Marker({
+            color: "red",
+            rotation: 0
+          })
+            .setLngLat([peak["long"], peak["lat"]])
+            .addTo(map)
+        })
+      });
       const nav = new mapboxgl.NavigationControl({
         visualizePitch: true,
         showZoom: true,
@@ -40,6 +57,8 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <hr>
+    <!-- {{ peaks }} -->
     <!-- <button v-on:click="makeMap()">Make Map</button> -->
     <div id='map' style='width: 1000px; height: 800px;'></div>
 
