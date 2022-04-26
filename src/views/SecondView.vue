7325a304be0ba63874a8e6a541e1c1ca5ac7cb1a
TLCurd <TLCurdChiro@gmail.com>
2022-04-25 20:31:46 -0600

    add SecondView for map to experiment with setting up popups w/ information and clickable image to link to peak show page

--- src/views/SecondView.vue
+++ src/views/SecondView.vue
@@ -32,13 +32,23 @@ export default {
       axios.get(`http://localhost:3000/peaks.json`).then(response => {
         console.log(response.data);
         this.peaks = response.data;
+        var description = "";
         this.peaks.forEach(peak => {
-          console.log(this.peaks.count)
+          console.log(this.peaks.count);
+          description = `<strong>${peak.name}</strong><p> <a href="http://localhost:8080/peaks/${peak.id}"><img src=${peak.photo} target="_blank" title="Learn more about ${peak.name}"></a> <p>Located in the ${peak.range} mountain range</p>
+          <p>Elevation:${peak.elevation}</p>
+          <p>Official 14er: ${peak.official_14er}</p>
+          <p>The summit is ${peak.prominence} above the nearest peak.</p></p>`;
           const marker = new mapboxgl.Marker({
             color: "red",
-            rotation: 0
+            rotation: 0,
           })
             .setLngLat([peak.long, peak.lat])
+            .setPopup(new mapboxgl.Popup({ offset: 25 }) //add popups
+              .setHTML(
+                description
+              )
+            )
             .addTo(map)
         })
       });
@@ -66,4 +76,25 @@ export default {
 </template>
 
 <style>
+img {
+  width: 200px
+}
+
+.mapboxgl-popup {
+  max-width: 400px;
+}
+
+.mapboxgl-popup-content {
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+}
+
+marker {
+  background-image: url('https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg');
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer
+}
 </style>
